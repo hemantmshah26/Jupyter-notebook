2cb8d0941027792ac051b8c9873d6fa0b1072f93
hemantmshah26 <hemant.m.shah@gmail.com>
2017-06-20 17:04:14 -0700

    Create Read large files in excel.py

---
+++ Read large files in excel.py
@@ -0,0 +1,2 @@
+# Python lets you use OS-X/Linux slashes "/" in Windows. This feature helps avousing double slashes "\\" needed to specify direcotry location in Windows.
+Test = pd.read_csv("C:/Users/Test/Python/File.csv")
